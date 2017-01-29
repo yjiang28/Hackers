59c88ea760985f096ad6fc34fb51968e053a7c61
amadrzyk <alexmadrzyk@gmail.com>
2017-01-29 08:44:57 -0500

    Testing on Heroku

---
+++ public_html/browser/www/js/keys.js
@@ -0,0 +1,13 @@
+// keys.js
+// Contains keys to run this demo
+// The keys should be kept secret and separate from the code
+// Enter your keys below to take the first in running this step 
+// VROOOOM VROOM
+
+// Returns an object with the required credentials
+function getKeys () {
+	return {
+		CLARIFAI_CLIENT_ID: "jC6GRWmkx85P_lyVyEu1t0GP9EEEKYEbw6by7IRN",
+		CLARIFAI_CLIENT_SECRET: "x9CN4t3rnzXdFcSTqjaIQkYnLv5r4uYvcTaNQCI2"
+	}
+}
---
+++ public_html/index.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="refresh" content="0; url=browser/www/index.html" />
+	</head>
+</html>
