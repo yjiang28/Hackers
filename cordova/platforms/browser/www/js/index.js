af3199fa1d823e2e0e5d92d1adc34e0ef1101450
amadrzyk <alexmadrzyk@gmail.com>
2017-01-29 12:51:55 -0500

    Finished

--- cordova/platforms/browser/www/js/index.js
+++ cordova/platforms/browser/www/js/index.js
@@ -41,5 +41,108 @@ function animation() {
     }
     init();
 
-    $('#theme').trigger('change');
+	// Finding a bunch of elements in the DOM
+	var app = $(".app")
+	var imageInput = $("#imageUrl");
+	var submitButton = $("#submitBtn");
+	var image = $("#image");
+	var tagsContainer = $(".tags-container");
+	var tags = $(".tags")
+
+	/*
+	 * displayTag
+	 * functionality to display the tag with classes and probabilities
+	 * contains some functionality regarding the response manipulation
+	 */
+	function displayTag (response) {
+		console.log("Clarifai Response!")
+		console.log(response)
+
+		/*
+		 * an array of resultant images is received
+		 * in this case we only request one
+		 * so we just need to get the first one
+		 */
+		var image 	= 	response.results[0];
+
+		// Image has a further tag that contains classes, concept_ids and
+		// probabilities for each concept
+		var tag 	= 	image.result.tag;
+		var word = tag["classes"][0];
+
+		console.log(word);
+
+
+		// Looping through all the classes in the tag using map
+		// to get the html for each concept
+		var concepts = tag.classes.slice(0,1).map(function (value) {
+			// adding class and concept information
+			return `<h3>${value}</h3>`;
+		});
+
+		// joining all the stuff generated and throwing the html into .tags
+		tags.html(concepts.join(""));
+
+		// displaying the hidden container
+		tagsContainer.show()
+
+	}
+
+
+	/*
+	 * assignColor
+	 * returns the color for progress bar based on the probability
+	 */
+	function assignColor (prob, index, length) {
+		if (prob > 0.9) {
+			return "success"
+		}
+		else if (prob > 0.85 && prob <= 0.9) {
+			return "info"
+		}
+		else {
+			return "warning"
+		}
+	}
+
+	// In case of error displays an error in the Clarifai
+	function displayError (error) {
+
+		// Preparing the error message
+		var errorMessage = "<p>" + error.status_msg + "</p>"
+		var errorHtml = "<div class='errorBox'><h1>Error ❌</h1>" + errorMessage + "</div>"
+
+		// throwing the errorHtml in .tags
+		tags.html(errorHtml)
+		tagsContainer.show()
+	}
+
+
+	// function to initialize the keys
+	function initialize () {
+		// getting the credential through calling getKeys()
+		// which is available in Global scope because of keys.js
+		var keys = getKeys() || {}
+
+		var clientId = keys["CLARIFAI_CLIENT_ID"]
+		var clientSecret = keys["CLARIFAI_CLIENT_SECRET"]
+
+		if (!clientId || !clientSecret) {
+			app.html("Enter your Clarifai's Client ID and Client Secret in order to successfully run this demo. Go to developer.clarifai.com, sign up and create your application if you haven't already. You'll have to edit keys.js file to enter your credentials")
+		}
+		else {
+			// TODO
+			// Initialize the Clarifai api here
+				app.show()
+				Clarifai.initialize({
+				clientId: clientId,
+				clientSecret: clientSecret
+				})
+
+		}
+
+
+	}
+	$('#theme').trigger('change');
+
 })(mobiscroll.$);
---
+++ cordova/platforms/browser/www/vserver.js
@@ -0,0 +1,74 @@
+/**
+ * Created by yuech on 1/29/2017.
+ */
+/**
+ * Created by yuech on 1/28/2017.
+ */
+var http = require('http');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
+//Lets define a port we want to listen to
+const PORT=3000;
+
+//We need a function which handles requests and send response
+function handleRequest(req, res){
+    var url = req.url;
+    var method = req.method;
+    console.log("dirname: "+path.join(__dirname,url));
+    console.log(method);
+    switch(req.method)
+    {
+        case 'GET':
+            //console.log("get method");
+            if (url == '/') {
+                //console.log("slash");
+                renderFile(res, path.join(__dirname,'video.html'));
+            }
+            /*else if(url == '/upload.html'){
+                console.log("option");
+                renderFile(res, path.join(__dirname,'upload.html'));
+            }
+            */
+            // else if (url == '/login') {console.log(path.join(__dirname, 'public','login.html'));renderFile(res, path.join(__dirname,'public','login.html'));console.log('read file end');}
+            // else if (url == '/register') renderFile(res, path.join(__dirname,'public','register.html'));
+            else renderFile(res, path.join(__dirname, url));
+            break;
+        case 'POST':
+            if (url=='url'){
+                req.data
+            }
+
+            break;
+        default:
+            console.log('method has not according solution');
+    }}
+
+function renderFile(res, file)
+{
+    console.log('render file');
+    fs.readFile(file, function(err, data){
+        if(err)
+        {
+            res.writeHead(404);
+            res.write('File Not Found');
+        }
+        else
+        {
+            res.writeHead(200);
+            res.write(data);
+        }
+        res.end();
+    });
+}
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
